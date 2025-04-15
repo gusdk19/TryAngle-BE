bea3d39f046d282a715311c2bc696ff9423d8992
Hyunah Lee <hyuna030619@naver.com>
2025-04-15 21:08:30 +0900

    feat: report 엔티티 생성

--- src/main/java/capstone/TryAngle/TryAngleApplication.java
+++ src/main/java/capstone/TryAngle/TryAngleApplication.java
@@ -2,8 +2,10 @@ package capstone.TryAngle;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class TryAngleApplication {
 
 	public static void main(String[] args) {
