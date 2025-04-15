13901563c8fcc9d1f7d8277a075617ab65333c20
Hyunah Lee <hyuna030619@naver.com>
2025-04-15 19:54:15 +0900

    feat: initial setup

---
+++ src/main/java/capstone/TryAngle/TryAngleApplication.java
@@ -0,0 +1,13 @@
+package capstone.TryAngle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TryAngleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TryAngleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/capstone/TryAngle/TryAngleApplicationTests.java
@@ -0,0 +1,13 @@
+package capstone.TryAngle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TryAngleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
