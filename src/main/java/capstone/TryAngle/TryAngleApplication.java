2ecdf41ca8bcc7167b5cad82430a324536c8cd15
Hyunah Lee <hyuna030619@naver.com>
2025-05-02 11:57:26 +0900

    feat: spring security 활성화

--- src/main/java/capstone/TryAngle/TryAngleApplication.java
+++ src/main/java/capstone/TryAngle/TryAngleApplication.java
@@ -2,12 +2,10 @@ package capstone.TryAngle;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-// 스프링 시큐리티 잠시 꺼둠
-@SpringBootApplication(exclude= SecurityAutoConfiguration.class)
+@SpringBootApplication
 @EnableJpaAuditing
 @ComponentScan(basePackages = {"capstone.TryAngle", "capstone"})
 public class TryAngleApplication {
