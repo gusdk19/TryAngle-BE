4f191982daa20417ea2bf927f4532dce4afd95f7
Hyunah Lee <hyuna030619@naver.com>
2025-05-05 02:22:35 +0900

    fix: globalException 반환 로직 수정

--- src/main/java/capstone/TryAngle/common/ApiResponse.java
+++ src/main/java/capstone/TryAngle/common/ApiResponse.java
@@ -33,7 +33,7 @@ public class ApiResponse<T> {
 
 
     // 실패한 경우 응답 생성
-    public static <T> ApiResponse<T> onFailure(String code, String message, T data) {
-        return new ApiResponse<>(false, code, message, data);
+    public static <T> ApiResponse<T> onFailure(String code, String message, T result) {
+        return new ApiResponse<>(false, code, message, result);
     }
 }
