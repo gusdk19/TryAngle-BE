7c13bab122e0aec70447595594eb3ecb77fc12d4
Hyunah Lee <hyuna030619@naver.com>
2025-05-19 14:34:41 +0900

    feat: SuccessStatus code 수정

--- src/main/java/capstone/TryAngle/common/status/SuccessStatus.java
+++ src/main/java/capstone/TryAngle/common/status/SuccessStatus.java
@@ -13,11 +13,13 @@ public enum SuccessStatus implements BaseCode {
     _OK(HttpStatus.OK, "COMMON200", "요청에 성공했습니다."),
 
     // User 관련 응답
-    SIGNUP_SUCCESS(HttpStatus.OK, "COMMON200", "회원가입에 성공했습니다."),
-    AVAILABLE_EMAIL(HttpStatus.OK, "COMMON200", "사용 가능한 이메일입니다."),
-    AVAILABLE_NICKNAME(HttpStatus.OK, "COMMON200", "사용 가능한 닉네임입니다."),
-    LOGIN_SUCCESS(HttpStatus.OK, "COMMON200", "로그인에 성공했습니다."),
-    MODIFY_SUCCESS(HttpStatus.OK, "COMMON200", "회원 정보 수정에 성공했습니다.");
+    SIGNUP_SUCCESS(HttpStatus.OK, "USER200", "회원가입에 성공했습니다."),
+    AVAILABLE_EMAIL(HttpStatus.OK, "USER200", "사용 가능한 이메일입니다."),
+    AVAILABLE_NICKNAME(HttpStatus.OK, "USER200", "사용 가능한 닉네임입니다."),
+    LOGIN_SUCCESS(HttpStatus.OK, "USER200", "로그인에 성공했습니다."),
+    MODIFY_SUCCESS(HttpStatus.OK, "USER200", "회원 정보 수정에 성공했습니다."),
+    FOLLOW_SUCCESS(HttpStatus.OK, "USER200", "팔로우에 성공했습니다."),
+    UNFOLLOW_SUCCESS(HttpStatus.OK, "USER200", "언팔로우에 성공했습니다.");
 
     private final HttpStatus httpStatus;
     private final String code;
