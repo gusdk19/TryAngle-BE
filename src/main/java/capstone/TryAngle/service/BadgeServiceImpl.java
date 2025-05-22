1ada60fdb5856e7ba162c9b8effa5d24ee713b27
Hyunah Lee <hyuna030619@naver.com>
2025-05-23 00:00:57 +0900

    feat: 뱃지 이름 칼럼 추가

--- src/main/java/capstone/TryAngle/service/BadgeServiceImpl.java
+++ src/main/java/capstone/TryAngle/service/BadgeServiceImpl.java
@@ -8,11 +8,15 @@ import capstone.TryAngle.model.user.UserBadge;
 import capstone.TryAngle.repository.BadgeRepository;
 import capstone.TryAngle.repository.UserBadgeRepository;
 import capstone.TryAngle.repository.UserRepository;
+import capstone.TryAngle.web.converter.UserConverter;
+import capstone.TryAngle.web.dto.UserResponseDTO;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -25,19 +29,19 @@ public class BadgeServiceImpl implements BadgeService {
 
     public void initializeBadges() {
         List<Badge> badges = List.of(
-                new Badge("/images/badges/badge1.png", "가입 후 첫 챌린지 참여", "가입 후 첫 챌린지 참여"),
-                new Badge("/images/badges/badge2.png", "첫 챌린지 성공", "챌린지 1회 참여 완료"),
-                new Badge("/images/badges/badge3.png", "첫 팔로워 추가", "첫 팔로워 추가"),
-                new Badge("/images/badges/badge4.png", "챌린지 3회 이상 성공", "챌린지 3회 이상 성공"),
-                new Badge("/images/badges/badge5.png", "7회 연속 챌린지 성공", "7회 연속 챌린지 성공"),
-                new Badge("/images/badges/badge6.png", "7일 이상 챌린지 연속 참여", "7일 이상 챌린지 연속 참여"),
-                new Badge("/images/badges/badge7.png", "총 15회 이상 챌린지 참여", "총 15회 이상 챌린지 참여"),
-                new Badge("/images/badges/badge8.png", "한 달 간 매일 챌린지 참여", "한 달 간 매일 챌린지 참여"),
-                new Badge("/images/badges/badge9.png", "챌린지 30개 이상 참여 완료", "챌린지 30개 이상 참여 완료"),
-                new Badge("/images/badges/badge10.png", "50회 이상 챌린지 완료", "50회 이상 챌린지 완료"),
-                new Badge("/images/badges/badge11.png", "100일 이상 챌린지 연속 참여 및 완료", "100일 이상 챌린지 연속 참여 및 완료"),
-                new Badge("/images/badges/badge12.png", "그룹 챌린지에서 리더 역할 수행", "그룹 챌린지에서 리더 역할 수행"),
-                new Badge("/images/badges/badge13.png", "10명 이상의 다른 참여자에게 챌린지 권유 및 참여 유도", "10명 이상의 다른 참여자에게 챌린지 권유 및 참여 유도")
+                new Badge("/images/badges/badge1.png", "새싹 챌린저", "가입 후 첫 챌린지 참여", "가입 후 첫 챌린지 참여"),
+                new Badge("/images/badges/badge2.png", "열정의 시작", "첫 챌린지 성공", "챌린지 1회 참여 완료"),
+                new Badge("/images/badges/badge3.png", "첫 팔로워", "첫 팔로워 추가", "첫 팔로워 추가"),
+                new Badge("/images/badges/badge4.png", "도전자 챌린저", "챌린지 3회 이상 성공", "챌린지 3회 이상 성공"),
+                new Badge("/images/badges/badge5.png", "연속 성공 챌린저", "7회 연속 챌린지 성공", "7회 연속 챌린지 성공"),
+                new Badge("/images/badges/badge6.png", "열정 챌린저", "7일 이상 챌린지 연속 참여", "7일 이상 챌린지 연속 참여"),
+                new Badge("/images/badges/badge7.png", "꾸준 챌린저", "총 15회 이상 챌린지 참여", "총 15회 이상 챌린지 참여"),
+                new Badge("/images/badges/badge8.png", "개근왕", "한 달 간 매일 챌린지 참여", "한 달 간 매일 챌린지 참여"),
+                new Badge("/images/badges/badge9.png", "챌린지 중독자", "챌린지 30개 이상 참여 완료", "챌린지 30개 이상 참여 완료"),
+                new Badge("/images/badges/badge10.png", "마스터 챌린저", "50회 이상 챌린지 완료", "50회 이상 챌린지 완료"),
+                new Badge("/images/badges/badge11.png", "전설 챌린저", "100일 이상 챌린지 연속 참여 및 완료", "100일 이상 챌린지 연속 참여 및 완료"),
+                new Badge("/images/badges/badge12.png", "리더 챌린저", "그룹 챌린지에서 리더 역할 수행", "그룹 챌린지에서 리더 역할 수행"),
+                new Badge("/images/badges/badge13.png", "챌린지 수호자", "10명 이상의 다른 참여자에게 챌린지 권유 및 참여 유도", "10명 이상의 다른 참여자에게 챌린지 권유 및 참여 유도")
         );
 
         badgeRepository.saveAll(badges);
